c8a017765bd56279ff57b86d3d64c5f943932c78
mark-morelos <makoimorelos@gmail.com>
2020-09-25 19:26:09 -0700

    added Sprint Challenge for Sprint 2

--- module2-sql-for-analysis/study_part1.py
+++ module2-sql-for-analysis/study_part1.py
@@ -5,16 +5,7 @@ from psycopg2.extras import execute_values
 import pandas
 import sqlite3
 
-DB_NAME = 'fottqjfu'
-DB_USER = 'fottqjfu'
-DB_PASS = 'UwC5Ky5r8IN4QRDdqPZIJJ2VhAFyyZ8v'
-DB_HOST = 'lallah.db.elephantsql.com'
-
-# Connect to ElephantSQL - hosted PostgreSQL DB
-conn = psycopg2.connect(dbname=DB_NAME,
-                        user=DB_USER,
-                        password=DB_PASS,
-                        host=DB_HOST)
+conn = sqlite3.connect('study_part1.sqlite3')
 
 cursor = conn.cursor()
 
@@ -32,3 +23,5 @@ cursor.execute(sql)
 insertion_query = f"INSERT INTO table_1 (student, studied, grade, age, sex) VALUES ('Lion-O', 'True', 85, 24, 'Male'), ('Cheetara', 'True', 95, 22, 'Female'), ('Mumm-Ra', 'False', 65, 153, 'Male'), ('Snarf', 'False', 70, 15, 'Male'), ('Panthro', 'True', 80, 30, 'Male');"
 
 cursor.execute(insertion_query)
+
+conn.commit()
